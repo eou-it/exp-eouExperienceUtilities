923d5b613895a6b04cc6d677dbe86866061d4358
Alex <10479505+alexkygar@users.noreply.github.com>
2025-08-25 07:55:11 -0700

    Fix name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: {
-      name: 'eouUtilities',
+      name: 'eou-experience-utilities',
       type: 'umd'
     },
     globalObject: 'this'
